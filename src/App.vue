8c0a31f0972bb246a0a8ab5fae3b57528cdbd939
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-01-21 12:53:38 -0400

    Feat: Add Header in Root vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from "./components/TheHeader.vue";
+import { RouterView } from "vue-router";
+</script>
 
 <template>
-  <div></div>
+  <TheHeader />
+  <RouterView />
 </template>
 
 <style scoped></style>
