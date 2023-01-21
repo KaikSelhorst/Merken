a04846d6cf8901152700a1be81affd5b261de939
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-01-21 16:37:11 -0400

    New: Component for Input markdown for tranform in HTML

---
+++ src/components/MarkdownInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps(["content"]);
+defineEmits(["update:content"]);
+</script>
+
+<template>
+  <textarea
+    :value="content"
+    @input="
+      $emit('update:content', ($event.target as HTMLTextAreaElement).value)
+    "
+  >
+  </textarea>
+</template>
+
+<style scoped>
+textarea {
+  border: 1px solid rgba(84, 84, 84, 0.48);
+  padding: 8px 12px;
+  font-size: 1rem;
+  background: #2f2f2f;
+  resize: none;
+  color: rgba(255, 255, 255, 0.87);
+  outline: none;
+}
+</style>
