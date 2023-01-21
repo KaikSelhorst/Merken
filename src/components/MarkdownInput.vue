935b9af5a9d79a99b8e68e92611790e4d54b9ca4
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-01-21 16:58:55 -0400

    Fix: add border-radius

--- src/components/MarkdownInput.vue
+++ src/components/MarkdownInput.vue
@@ -18,6 +18,7 @@ textarea {
   border: 1px solid rgba(84, 84, 84, 0.48);
   padding: 8px 12px;
   font-size: 1rem;
+  border-radius: 2px;
   background: #2f2f2f;
   resize: none;
   color: rgba(255, 255, 255, 0.87);
