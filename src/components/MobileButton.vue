6e4140034b8ce56a2cf6b46c96d51119a7b93985
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-01-28 15:41:48 -0400

    Feat: new settings view for setting up workspaces

---
+++ src/components/MobileButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import emitter from "@/emitter";
+import { outsideClick } from "@/helpers";
+
+const props = defineProps({
+  emittedName: {
+    type: String,
+    required: true,
+  },
+});
+
+const emitterMobileEvent = () => emitter.emit(props.emittedName);
+const onClick = (event: MouseEvent) => {
+  const clickElement = event.currentTarget;
+  if (clickElement && clickElement instanceof HTMLElement) {
+    emitterMobileEvent();
+    outsideClick(clickElement, ["click", "touchstart"], emitterMobileEvent);
+  }
+};
+</script>
+
+<template>
+  <button @click="onClick">Menu</button>
+</template>
+<style scoped>
+button {
+  color: rgb(235 235 235 / 60%);
+  font-weight: 500;
+  font-size: 0.75rem;
+  display: flex;
+  align-items: center;
+}
+button::before {
+  content: "";
+  display: inline-block;
+  background: currentColor;
+  height: 2px;
+  transition: transform 0.3s;
+  width: 12px;
+  margin-right: 4px;
+  box-shadow: 0 4px, 0 -4px;
+}
+</style>
