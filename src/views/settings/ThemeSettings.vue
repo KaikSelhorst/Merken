ed059e06d582fe851d6c68a0c89d06dcb949187c
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-02-02 09:05:01 -0400

    Fix: add active theme indicaticator

--- src/views/settings/ThemeSettings.vue
+++ src/views/settings/ThemeSettings.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import ButtonLarge from "@/components/buttons/ButtonLarge.vue";
+import { ref } from "vue";
+import { getLocal } from "@/helpers";
+import type { UserConfig } from "env";
 import emitter from "@/emitter";
+
+import ButtonLarge from "@/components/buttons/ButtonLarge.vue";
 
 const themes = [
   "Nord",
@@ -14,7 +18,9 @@ const themes = [
 
 const onClick = (theme: string) => {
   emitter.emit("UPDATE_USER_CONFIG", { theme });
+  activeTheme.value = theme;
 };
+const activeTheme = ref(getLocal<UserConfig>("config").theme);
 </script>
 
 <template>
@@ -23,9 +29,20 @@ const onClick = (theme: string) => {
       <h1>Theme</h1>
       <ul class="grid">
         <li v-for="theme in themes" :key="theme">
-          <ButtonLarge @click="onClick(theme)">{{ theme }}</ButtonLarge>
+          <ButtonLarge
+            @click="onClick(theme)"
+            :class="{
+              active: theme.toLowerCase() == activeTheme.toLocaleLowerCase(),
+            }"
+            >{{ theme }}</ButtonLarge
+          >
         </li>
       </ul>
     </article>
   </div>
 </template>
+<style scoped>
+button.active {
+  background: var(--mint);
+}
+</style>
