6e9028b636ac504f09e3ff8d5a57bbb183020175
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-02-03 10:52:12 -0400

    Feat: Color highlight using the Highlight.js library
    in the index.html add link to style code of github
    WorkBranch.vue configure highlight using setOptions of marked

--- src/components/WorkBranch.vue
+++ src/components/WorkBranch.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { marked } from "marked";
+import hljs from "highlight.js";
 
+marked.setOptions({
+  highlight: function (code, lang) {
+    const language = hljs.getLanguage(lang) ? lang : "plaintext";
+    return hljs.highlight(code, { language }).value;
+  },
+});
 const props = defineProps(["content"]);
 defineEmits(["update:content"]);
 
