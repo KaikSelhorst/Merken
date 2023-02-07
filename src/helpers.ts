fce5d4e2cd5976bc407e9278a82f88cd9b2b0690
Kaik Selhorst <contato.kaikselhorst@gmail.com>
2023-02-06 20:33:29 -0400

    New: Romanize Function

--- src/helpers.ts
+++ src/helpers.ts
@@ -40,3 +40,29 @@ export const outsideClick = (
     element.setAttribute(outside, "");
   }
 };
+
+export const romanize = (num: number) => {
+  let roman = "";
+  const lookup: { [key: string]: number } = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  for (const i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+};
